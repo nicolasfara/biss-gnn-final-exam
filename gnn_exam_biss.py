92d89475a41979044e17546999d3183032f9eb71
Nicolas Farabegoli <nicolas.farabegoli@gmail.com>
2024-07-17 17:22:03 +0200

    Add fixed GNN code

---
+++ gnn_exam_biss.py
@@ -0,0 +1,311 @@
+import numpy as np  # linear algebra
+import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
+import matplotlib.pyplot as plt
+import seaborn as sns
+import torch
+import os
+import zipfile
+import urllib.request
+from torch import Tensor
+import torch.nn.functional as F
+from torch_geometric import seed_everything
+from torch_geometric.data import HeteroData
+from torch_geometric.nn import SAGEConv, to_hetero
+from torch_geometric.loader import LinkNeighborLoader
+import torch_geometric.transforms as T
+import tqdm
+from sklearn.metrics import roc_auc_score
+import matplotlib.pyplot as plt
+
+GCN_LAYERS = 3  # Number of GCN Layers # modificato ale
+HIDDEN_DIM = 64  # Number of hidden neurons in each GCN Layer
+learning_rate = 1e-3
+weight_decay = 5e-4
+epochs = 10
+batch_Size = 128
+negative_samples = 3
+random_seed = 42
+
+seed_everything(random_seed)
+
+params = {'legend.fontsize': 'medium',
+          'figure.figsize': (10, 8),
+          'figure.dpi': 100,
+          'axes.labelsize': 'medium',
+          'axes.titlesize': 'medium',
+          'xtick.labelsize': 'medium',
+          'ytick.labelsize': 'medium'}
+plt.rcParams.update(params)
+
+# check if datasets folder exists
+if not os.path.exists("datasets"):
+    urllib.request.urlretrieve("https://files.grouplens.org/datasets/movielens/ml-latest-small.zip",
+                               "ml-latest-small.zip")
+    with zipfile.ZipFile("ml-latest-small.zip", "r") as zip_ref:
+        zip_ref.extractall("datasets")
+
+movies_path = './datasets/ml-latest-small/movies.csv'
+ratings_path = './datasets/ml-latest-small/ratings.csv'
+
+ratings_df = pd.read_csv(ratings_path, usecols=['userId', 'movieId', 'rating'])
+movies_df = pd.read_csv(movies_path, usecols=['movieId', 'title', 'genres'])
+
+sns.countplot(x='rating', data=ratings_df)
+plt.show()
+
+# Split genres and convert into indicator variables:
+genres = movies_df['genres'].str.get_dummies('|')
+print(genres[["Action", "Adventure", "Drama", "Horror"]].head())
+# Use genres as movie input features:
+movie_feat = torch.from_numpy(genres.values).to(torch.float)
+assert movie_feat.size() == (9742, 20)  # 9742 movies and 20 genres in total.
+
+# Create a mapping from unique user indices to range [0, num_user_nodes):
+unique_user_id = ratings_df['userId'].unique()
+unique_user_id = pd.DataFrame(data={
+    'userId': unique_user_id,
+    'mappedID': pd.RangeIndex(len(unique_user_id)),
+})
+print("Mapping of user IDs to consecutive values:")
+print("==========================================")
+print(unique_user_id.head())
+print()
+
+# Create a mapping from unique movie indices to range [0, num_movie_nodes):
+unique_movie_id = ratings_df['movieId'].unique()
+unique_movie_id = pd.DataFrame(data={
+    'movieId': unique_movie_id,
+    'mappedID': pd.RangeIndex(len(unique_movie_id)),
+})
+print("Mapping of movie IDs to consecutive values:")
+print("===========================================")
+print(unique_movie_id.head())
+# Perform merge to obtain the edges from users and movies:
+ratings_user_id = pd.merge(ratings_df['userId'], unique_user_id,
+                           left_on='userId', right_on='userId', how='left')
+ratings_user_id = torch.from_numpy(ratings_user_id['mappedID'].values)
+ratings_movie_id = pd.merge(ratings_df['movieId'], unique_movie_id,
+                            left_on='movieId', right_on='movieId', how='left')
+ratings_movie_id = torch.from_numpy(ratings_movie_id['mappedID'].values)
+# With this, we are ready to construct our `edge_index` in COO format
+# following PyG semantics:
+edge_index_user_to_movie = torch.stack([ratings_user_id, ratings_movie_id], dim=0)
+assert edge_index_user_to_movie.size() == (2, 100836)
+
+print()
+print("Final edge indices pointing from users to movies:")
+print("=================================================")
+print(edge_index_user_to_movie)
+
+# Use rating as edge features: ale
+edge_rating = torch.from_numpy((ratings_df['rating'].to_numpy())).to(torch.float)
+
+print(edge_rating)
+print(edge_rating.size())
+
+data = HeteroData()
+# Save node indices:
+data["user"].node_id = torch.arange(len(unique_user_id))
+data["movie"].node_id = torch.arange(len(movies_df))
+# Add the node features and edge indices:
+data["movie"].x = movie_feat
+data["user", "rates", "movie"].edge_index = edge_index_user_to_movie
+
+# add edge features -> considerare le feature e' opzionale nel task del prof
+data["user", "rates", "movie"].edge_weight = edge_rating
+
+# We also need to make sure to add the reverse edges from movies to users
+# in order to let a GNN be able to pass messages in both directions.
+# We can leverage the `T.ToUndirected()` transform for this from PyG:
+data = T.ToUndirected()(data)
+
+# For this, we first split the set of edges into
+# training (80%), validation (10%), and testing edges (10%).
+# Across the training edges, we use 70% of edges for message passing,
+# and 30% of edges for supervision.
+# We further want to generate fixed negative edges for evaluation with a ratio of 2:1.
+# Negative edges during training will be generated on-the-fly.
+# We can leverage the `RandomLinkSplit()` transform for this from PyG:
+transform = T.RandomLinkSplit(
+    num_val=0.1,
+    num_test=0.1,
+    disjoint_train_ratio=0.3,
+    neg_sampling_ratio=negative_samples,
+    add_negative_train_samples=False,
+    edge_types=("user", "rates", "movie"),
+    rev_edge_types=("movie", "rev_rates", "user"),
+)
+train_data, val_data, test_data = transform(data)
+
+# In the first hop, we sample at most 20 neighbors.
+# In the second hop, we sample at most 10 neighbors.
+# In addition, during training, we want to sample negative edges on-the-fly with
+# a ratio of 2:1.
+# We can make use of the `loader.LinkNeighborLoader` from PyG:
+
+# Define seed edges:
+edge_label_index = train_data["user", "rates", "movie"].edge_label_index
+edge_label = train_data["user", "rates", "movie"].edge_label
+train_loader = LinkNeighborLoader(
+    data=train_data,
+    num_neighbors=[20, 10],
+    neg_sampling_ratio=negative_samples,
+    edge_label_index=(("user", "rates", "movie"), edge_label_index),
+    edge_label=edge_label,
+    weight_attr='edge_weight',
+    batch_size=batch_Size,
+    shuffle=True,
+)
+
+class GNN(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super().__init__()
+        self.conv1 = SAGEConv(hidden_channels, hidden_channels)
+        self.conv2 = SAGEConv(hidden_channels, hidden_channels)
+
+    def forward(self, x: Tensor, edge_index: Tensor) -> Tensor:
+        x = F.relu(self.conv1(x, edge_index))
+        x = self.conv2(x, edge_index)
+        return x
+
+
+# Our final classifier applies the dot-product between source and destination
+# node embeddings to derive edge-level predictions:
+class Classifier(torch.nn.Module):
+    # def forward(self, x_user: Tensor, x_movie: Tensor, edge_label_index: Tensor) -> Tensor:
+    def forward(self, x_user: Tensor, x_movie: Tensor, edge_label_index: Tensor, edge_weight: Tensor) -> Tensor:
+        # Convert node embeddings to edge-level representations:
+        edge_feat_user = x_user[edge_label_index[0]]
+        edge_feat_movie = x_movie[edge_label_index[1]]
+        # Apply dot-product to get a prediction per supervision edge:
+        # return (edge_feat_user * edge_feat_movie * edge_weight[0]).sum(dim=-1) # non funziona perchè sono tutti i pesi.
+
+        return (edge_feat_user * edge_feat_movie).sum(dim=-1)
+
+
+class Model(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super().__init__()
+        # Since the dataset does not come with rich features, we also learn two
+        # embedding matrices for users and movies:
+        self.movie_lin = torch.nn.Linear(20, hidden_channels)
+        self.user_emb = torch.nn.Embedding(data["user"].num_nodes, hidden_channels)
+        self.movie_emb = torch.nn.Embedding(data["movie"].num_nodes, hidden_channels)
+        # Instantiate homogeneous GNN:
+        self.gnn = GNN(hidden_channels)
+        # Convert GNN model into a heterogeneous variant:
+        self.gnn = to_hetero(self.gnn, metadata=data.metadata())
+        self.classifier = Classifier()
+
+    def forward(self, data: HeteroData) -> Tensor:
+        x_dict = {
+            "user": self.user_emb(data["user"].node_id),
+            "movie": self.movie_lin(data["movie"].x) + self.movie_emb(data["movie"].node_id),
+        }
+        # `x_dict` holds feature matrices of all node types
+        # `edge_index_dict` holds all edge indices of all edge types
+        x_dict = self.gnn(x_dict, data.edge_index_dict)
+        pred = self.classifier(
+            x_dict["user"],
+            x_dict["movie"],
+            data["user", "rates", "movie"].edge_label_index,
+            data["user", "rates", "movie"].edge_weight,  # per ora non usato, serve?
+        )
+        return pred
+
+model = Model(hidden_channels=64)
+
+# Define the validation seed edges:
+edge_label_index = val_data["user", "rates", "movie"].edge_label_index
+edge_label = val_data["user", "rates", "movie"].edge_label
+val_loader = LinkNeighborLoader(
+    data=val_data,
+    num_neighbors=[20, 10],
+    edge_label_index=(("user", "rates", "movie"), edge_label_index),
+    edge_label=edge_label,
+    batch_size=batch_Size,
+    shuffle=False,
+)
+sampled_data = next(iter(val_loader))
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f"Device: '{device}'")
+
+train_losses = []
+val_losses = []
+val_aucs = []
+
+model = model.to(device)
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+for epoch in range(1, epochs):
+    total_loss = total_examples = 0
+    for sampled_data in tqdm.tqdm(train_loader):
+        optimizer.zero_grad()
+        sampled_data.to(device)
+        pred = model(sampled_data)
+        ground_truth = sampled_data["user", "rates", "movie"].edge_label
+        loss = F.binary_cross_entropy_with_logits(pred, ground_truth)
+        loss.backward()
+        optimizer.step()
+        total_loss += float(loss) * pred.numel()
+        total_examples += pred.numel()
+
+    avg_train_loss = total_loss / total_examples
+    train_losses.append(avg_train_loss)
+    print(f"Epoch: {epoch:02d}, Loss: {avg_train_loss:.4f}")
+
+    preds = []
+    ground_truths = []
+    for sampled_data in tqdm.tqdm(val_loader):
+        with torch.no_grad():
+            sampled_data.to(device)
+            pred = model(sampled_data)
+            preds.append(pred)
+            ground_truths.append(sampled_data["user", "rates", "movie"].edge_label)
+    pred = torch.cat(preds, dim=0).cpu()
+    ground_truth = torch.cat(ground_truths, dim=0).cpu()
+    auc = roc_auc_score(ground_truth.numpy(), pred.numpy())
+    loss = F.binary_cross_entropy_with_logits(pred, ground_truth)
+    val_losses.append(float(loss))
+    val_aucs.append(auc)
+    print(f"\nValidation AUC: {auc:.4f}")
+
+# Plot the training and validation losses:
+plt.plot(train_losses, label="Train Loss")
+plt.plot(val_losses, label="Validation Loss")
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
+
+# Plot the validation AUCs:
+plt.plot(val_aucs, label="Validation AUC")
+plt.xlabel("Epoch")
+plt.ylabel("AUC")
+plt.legend()
+plt.show()
+
+# Define the validation seed edges:
+edge_label_index = test_data["user", "rates", "movie"].edge_label_index
+edge_label = test_data["user", "rates", "movie"].edge_label
+test_loader = LinkNeighborLoader(
+    data=test_data,
+    num_neighbors=[20, 10],
+    edge_label_index=(("user", "rates", "movie"), edge_label_index),
+    edge_label=edge_label,
+    batch_size=batch_Size,
+    shuffle=False,
+)
+sampled_data = next(iter(test_loader))
+
+preds = []
+ground_truths = []
+for sampled_data in tqdm.tqdm(test_loader):
+    with torch.no_grad():
+        sampled_data.to(device)
+        preds.append(model(sampled_data))
+        ground_truths.append(sampled_data["user", "rates", "movie"].edge_label)
+pred = torch.cat(preds, dim=0).cpu().numpy()
+ground_truth = torch.cat(ground_truths, dim=0).cpu().numpy()
+auc = roc_auc_score(ground_truth, pred)
+print(f"Test AUC: {auc:.4f}")
