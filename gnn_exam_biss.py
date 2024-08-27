009749a4bce2497759381c00fd9169452691c480
Davide Domini <davide.domini@unibo.it>
2024-08-27 15:01:06 +0200

    feat: now gnn layers number is parametric

--- gnn_exam_biss.py
+++ gnn_exam_biss.py
@@ -16,6 +16,21 @@ import torch_geometric.transforms as T
 import tqdm
 import urllib.request
 import zipfile
+from torch.nn import ModuleList
+
+# some utility functions
+
+def head(l):
+    return l[0]
+
+def tail(l):
+    return l[1:]
+
+def last(l):
+    return l[-1]
+
+
+# GNN definition and training
 
 GCN_LAYERS = 3 # Number of GCN Layers
 hidden_channels = 64  # Number of hidden neurons in each GCN Layer
@@ -163,16 +178,18 @@ train_loader = LinkNeighborLoader(
 
 
 class GNN(torch.nn.Module):
-    def __init__(self, hidden_channels):
+    def __init__(self, hidden_channels, num_layers):
         super().__init__()
-        self.conv1 = SAGEConv(hidden_channels, hidden_channels)
-        self.conv2 = SAGEConv(hidden_channels, hidden_channels)
+        self.layers = ModuleList()
+        for _ in range(num_layers):
+            self.layers.append(SAGEConv(hidden_channels, hidden_channels))
 
     def forward(self, x: Tensor, edge_index: Tensor) -> Tensor:
-        x = F.relu(self.conv1(x, edge_index))
-        x = self.conv2(x, edge_index)
+        if len(self.layers) > 1:
+            for layer in tail(self.layers):
+                x = F.relu(layer(x, edge_index))
+        x = last(self.layers)(x, edge_index)
         return x
-
 
 # Our final classifier applies the dot-product between source and destination
 # node embeddings to derive edge-level predictions:
@@ -197,7 +214,7 @@ class Model(torch.nn.Module):
         self.user_emb = torch.nn.Embedding(data["user"].num_nodes, hidden_channels)
         self.movie_emb = torch.nn.Embedding(data["movie"].num_nodes, hidden_channels)
         # Instantiate homogeneous GNN:
-        self.gnn = GNN(hidden_channels)
+        self.gnn = GNN(hidden_channels, 2)
         # Convert GNN model into a heterogeneous variant:
         self.gnn = to_hetero(self.gnn, metadata=data.metadata())
         self.classifier = Classifier()
